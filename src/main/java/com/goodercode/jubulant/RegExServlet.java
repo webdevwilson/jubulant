14932b9ee7f621f9e61dccffc49f7f5de2a90b51
Kerry Wilson <kerry@allthingswilson.com>
2011-06-23 21:07:40 -0500

    Initial commit

---
+++ src/main/java/com/goodercode/jubulant/RegExServlet.java
@@ -0,0 +1,52 @@
+package com.goodercode.jubulant;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegExServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(final HttpServletRequest request, 
+						 final HttpServletResponse response) throws ServletException, IOException {
+		
+		final String regex = request.getParameter("r");					 
+		final String flags = request.getParameter("f");
+		final String text = request.getParameter("t");
+		final Matcher m = Pattern.compile(regex).matcher(text);
+		
+		final Map<String, String> jsonMe = new HashMap<String, String>();
+		
+		jsonMe.put("matches", toJson(m.matches()));
+		jsonMe.put("regex", toJson(regex));
+		
+		final StringBuilder groups = new StringBuilder();
+		for(Integer i = 0; i <= m.groupCount(); i++) {
+			groups.append(toJson(m.group(i))).append(",");
+		}
+		jsonMe.put("groups", "["+groups.toString().substring(0,groups.length()-1)+"]");
+	
+		response.setContentType("application/json");
+		
+		// output to response
+		final StringBuilder output = new StringBuilder();
+		for(final String key : jsonMe.keySet()) {
+			output.append("'").append(key).append("':").append(jsonMe.get(key)).append(",");
+		}
+		response.getWriter().write("{"+output.toString().substring(0,output.length()-1)+"}");
+		
+	}
+	
+	private String toJson(final Object obj) {
+		return "'"+obj.toString().replace("'", "\\'")+"'";
+	}
+	
+
+}
---
+++ src/test/java/com/goodercode/jubulant/RegExServletTest.java
@@ -0,0 +1,56 @@
+package com.goodercode.jubulant;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+public class RegExServletTest {
+
+	private MockHttpServletResponse response;
+	
+	private MockHttpServletRequest request;
+	
+	private RegExServlet servlet;
+	
+	@Before
+	public void setup() {
+		response = new MockHttpServletResponse();
+		request = new MockHttpServletRequest();
+		servlet = new RegExServlet();
+	}
+	
+	@Test
+	public void should_set_contentType_to_application_json() throws Exception {
+		final String r = "a(bc)d";
+		request.setParameter("r", r);
+		request.setParameter("f", "");
+		request.setParameter("t", "abcd");
+		
+		servlet.doGet(request, response);
+		
+		assertThat( response.getContentType(), equalTo("application/json") );
+	}
+	
+	@Test
+	public void should_run_regular_expression_and_output_result() throws Exception {
+		
+		final String r = "a(bc)d";
+		request.setParameter("r", r);
+		request.setParameter("f", "");
+		request.setParameter("t", "abcd");
+		
+		servlet.doGet(request, response);
+		
+		final JSONObject json = new JSONObject(response.getContentAsString());
+		assertThat( json.getString("regex"), equalTo(r));
+		assertThat( json.getBoolean("matches"), is(true) );
+		assertThat( json.getJSONArray("groups").length(), equalTo(2));
+		
+	}
+}
