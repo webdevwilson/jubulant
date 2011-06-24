5aad81b532a23d9ffd2937c79bd2baeabddb45e1
Kerry Wilson <kerry@allthingswilson.com>
2011-06-23 23:29:16 -0500

    Fixed no match exception

--- src/main/java/com/goodercode/jubulant/RegExServlet.java
+++ src/main/java/com/goodercode/jubulant/RegExServlet.java
@@ -13,40 +13,44 @@ import javax.servlet.http.HttpServletResponse;
 
 public class RegExServlet extends HttpServlet {
 
-	@Override
-	protected void doGet(final HttpServletRequest request, 
-						 final HttpServletResponse response) throws ServletException, IOException {
-		
-		final String regex = request.getParameter("r");					 
-		final String flags = request.getParameter("f");
-		final String text = request.getParameter("t");
-		final Matcher m = Pattern.compile(regex).matcher(text);
-		
-		final Map<String, String> jsonMe = new HashMap<String, String>();
-		
-		jsonMe.put("matches", toJson(m.matches()));
-		jsonMe.put("regex", toJson(regex));
-		
-		final StringBuilder groups = new StringBuilder();
-		for(Integer i = 0; i <= m.groupCount(); i++) {
-			groups.append(toJson(m.group(i))).append(",");
-		}
-		jsonMe.put("groups", "["+groups.toString().substring(0,groups.length()-1)+"]");
-	
-		response.setContentType("application/json");
-		
-		// output to response
-		final StringBuilder output = new StringBuilder();
-		for(final String key : jsonMe.keySet()) {
-			output.append("'").append(key).append("':").append(jsonMe.get(key)).append(",");
-		}
-		response.getWriter().write("{"+output.toString().substring(0,output.length()-1)+"}");
-		
-	}
-	
-	private String toJson(final Object obj) {
-		return "'"+obj.toString().replace("'", "\\'")+"'";
-	}
-	
+    @Override
+    protected void doGet(final HttpServletRequest request,
+            final HttpServletResponse response) throws ServletException, IOException {
 
+        final String regex = request.getParameter("r");
+        final String flags = request.getParameter("f");
+        final String text = request.getParameter("t");
+
+        final Map<String, String> jsonMe = new HashMap<String, String>();
+
+        try {
+            final Matcher m = Pattern.compile(regex).matcher(text);
+
+
+            jsonMe.put("matches", toJson(m.matches()));
+            jsonMe.put("regex", toJson(regex));
+
+            final StringBuilder groups = new StringBuilder();
+            for (Integer i = 0; i <= m.groupCount(); i++) {
+                groups.append(toJson(m.group(i))).append(",");
+            }
+            jsonMe.put("groups", "[" + groups.toString().substring(0, groups.length() - 1) + "]");
+        } catch (final IllegalStateException e) {
+            jsonMe.put("matches", toJson(false));
+        }
+
+        response.setContentType("application/json");
+
+        // output to response
+        final StringBuilder output = new StringBuilder();
+        for (final String key : jsonMe.keySet()) {
+            output.append("'").append(key).append("':").append(jsonMe.get(key)).append(",");
+        }
+        response.getWriter().write("{" + output.toString().substring(0, output.length() - 1) + "}");
+
+    }
+
+    private String toJson(final Object obj) {
+        return "'" + obj.toString().replace("'", "\\'") + "'";
+    }
 }
